3c6790cfe5bb402499f03c1e7db067333e9fa766
Tim Roy <42869464+timroy@users.noreply.github.com>
2020-10-29 15:11:14 -0400

    Update anon__in_class_survey.R

--- anon__in_class_survey.R
+++ anon__in_class_survey.R
@@ -5,7 +5,7 @@ library(texreg)
 library(corrplot)
 
 # Importing Data
-sv <- read_csv("Anonymous in-class survey (Responses) - Form responses 1.csv")
+sv <- read_csv("in-class-survey/Anonymous in-class survey (Responses) - Form responses 1.csv")
 
 sv <-
 sv %>% 
@@ -20,6 +20,7 @@ sv %>%
          WOMAN = `In terms of gender, do you identify as a woman?`,
          POS_Q = `State your level of agreement with the following statement: We can make claims about the social world from doing social science in the same way that we can make claims about nature in the natural sciences.`,
          PID_STR = `To what degree do you identify with an electoral party where it matters to you.`) %>% 
+  # creating an individual fixed effect variable
   mutate(fixed_effect = 1:nrow(sv))
 
 # get correlations
@@ -30,23 +31,25 @@ sv %>%
 ggplot(aes(x = LRSCALE, y = IPSCALE)) +
   geom_point(position = position_jitter(width = 0.05)) +
   # geom_point(position = position_nudge()) +
-  geom_smooth(method = "lm", color = "red") +
+  geom_smooth(method = "lm", color = "blue") +
   theme_classic() +
-  labs(x = "Left-wing, ")
+  labs(x = "Left-Right Scale", y = "Int-Pos Scale")
 
 # Run Multiple Regression
 
 # Interpretevist-Positivist Scale
-m1 <- lm(IPSCALE ~ LRSCALE + PRIV_WORK + PUB_WORK + NPR_PUB + PR_PUB + FLUENT + WOMAN + PID_STR, data = sv)
+m1 <- lm(IPSCALE ~ LRSCALE + POS_Q + PRIV_WORK + PUB_WORK + NPR_PUB + PR_PUB + FLUENT + WOMAN + PID_STR, data = sv)
 screenreg(m1)
 
 # With individual fixed effects
-m1_FE <- lm(IPSCALE ~ LRSCALE + PRIV_WORK + PUB_WORK + NPR_PUB + PR_PUB + FLUENT + WOMAN + PID_STR + factor(fixed_effect), data = sv)
+m1_FE <- lm(IPSCALE ~ LRSCALE + PRIV_WORK + PUB_WORK + NPR_PUB + PR_PUB + FLUENT + WOMAN + PID_STR + 
+              factor(fixed_effect), data = sv)
 screenreg(m1_FE)
 
 # Create hypothetical data to predict over 
 pred_dat <- expand.grid(
   LRSCALE = 1:10,
+  POS_Q = 3,
   PRIV_WORK = 1,
   PUB_WORK = 1,
   PR_PUB = 1,
@@ -63,5 +66,7 @@ plot_dat <- cbind(pred_dat, predict(m1, pred_dat, interval = "confidence"))
 ggplot() +
   geom_smooth(data = sv, aes(x = LRSCALE, y = IPSCALE), method = "lm") +
   geom_line(data = plot_dat, aes(x = LRSCALE, y = fit), color = "red") +
-  geom_ribbon(data = plot_dat, aes(x = LRSCALE, ymax = upr, ymin = lwr), alpha = 0.5, color = "red") +
-  geom_point(data = sv, aes(x = LRSCALE, y = IPSCALE))
+  geom_ribbon(data = plot_dat, aes(x = LRSCALE, ymax = upr, ymin = lwr), alpha = 0.2, color = "red") +
+  geom_point(data = sv, aes(x = LRSCALE, y = IPSCALE), position = position_jitter(width = 0.05)) +
+  theme_classic() +
+  labs(x = "Left-Right Scale", y = "Int-Pos Scale")
